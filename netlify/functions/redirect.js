a80f94847267d13967b6953c9fc8fd2d0c857cef
Chisquare7 <chijiokechibuike.f@gmail.com>
2025-02-13 19:36:49 +0100

    Remove all URL redirect files

--- netlify/functions/redirect.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 301,
-    headers: {
-      Location: "https://fantastic-manatee-096881.netlify.app/",
-    },
-    body: "Redirecting...",
-  };
-};
