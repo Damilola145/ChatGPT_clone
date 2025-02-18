fcd363c09365b43e6dc1d79266fda5dcc4c5cf02
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2025-02-18 03:13:46 +0100

    applied saved theme

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 import { themeButtonFile } from "./features/darkLight.js";
 import { handleOutgoingChat } from "./features/outgoingChat.js";
 import { deleteChatButton } from "./features/deleteChat.js";
+import { applySavedTheme } from "./features/darkLight.js";
 
 const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 const sendButton = document.querySelector("#send-btn");
+
+document.addEventListener("DOMContentLoaded", applySavedTheme);
 
 if (sendButton) {
   sendButton.addEventListener("click", handleOutgoingChat);
