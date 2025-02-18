05aa5ed4f70a1865d6e9178e19ee79a2b697aba5
ESTHER ISRAEL OKON <105334293+Smartbabbe@users.noreply.github.com>
2025-02-18 02:58:33 +0100

    Added functionalities for the theme button and delete button

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+import { themeButtonFile } from "./features/darkLight.js";
 import { handleOutgoingChat } from "./features/outgoingChat.js";
+import { deleteChatButton } from "./features/deleteChat.js";
 
+const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
 const sendButton = document.querySelector("#send-btn");
 
 if (sendButton) {
   sendButton.addEventListener("click", handleOutgoingChat);
 }
+
+if (themeButton) {
+  themeButton.addEventListener("click", themeButtonFile);
+};
+
+if (deleteButton) {
+  deleteButton.addEventListener("click", deleteChatButton);
+};
